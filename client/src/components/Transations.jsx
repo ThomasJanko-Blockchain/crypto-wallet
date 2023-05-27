ed78ae565011d5c3921caa4286b18a79eb117896
ThomasJanko <thomas78125@gmail.com>
2023-05-27 21:59:09 +0200

    first commit

---
+++ client/src/components/Transations.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Transations = () => {
+    return (
+        <div>
+            <h1>Transations</h1>
+        </div>
+    );
+}
+
+export default Transations;
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './transactions.json'
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x1d2fc128e4A2dFBFDcF00A1c61bE0844Edc37f35';
