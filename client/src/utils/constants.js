fa87ebd9c7b20e7bad2a8c0db0f7ead105cf731a
ThomasJanko <thomas78125@gmail.com>
2023-06-03 00:30:48 +0200

    deploy

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-import abi from './Transactions.json'
+import abi from './transactions.json'
 
 export const contractABI = abi.abi;
 export const contractAddress = '0x39B9857bFB482760293B73855116466bBbB0B292';
